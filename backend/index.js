eccbb88d886733bae0fea80f0e440de430ef930f
razibdash <avrorazib@gmail.com>
2025-08-26 03:40:53 +0600

    add bacend and frontend file structure

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const dotenv=require('dotenv')
+const express = require("express");
+const cors= require("cors");
+const bodyParser = require("body-parser");
+const cloudinary = require("cloudinary").v2;
+const app = express();
+
+dotenv.config()
+
+//Port
+const PORT = process.env.PORT || 5000;
+//CORS configuration
+cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+
+})
+
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+//app listening
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
