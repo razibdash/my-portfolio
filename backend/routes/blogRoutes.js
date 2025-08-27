51ddccba189e8c305f0df901e4c6769ff8a118d6
razibdash <avrorazib@gmail.com>
2025-08-28 00:12:29 +0600

    create blog context and custom hook

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -1,11 +1,12 @@
 const express=require("express");
 const {protect}=require("../middleware/authMiddleware");
+const { createBlog, getAllBlogs, getBlogById, updateBlog, deleteBlog } = require("../controllers/blogController");
 const router=express.Router();
 
-router.post("/blog",protect,blogController.createBlog);
-router.get("/blog",blogController.getAllBlogs);
-router.get("/blog/:id",protect,blogController.getBlogById);
-router.put("/blog/:id",protect,blogController.updateBlog);
-router.delete("/blog/:id",protect,blogController.deleteBlog);
+router.post("/blog",protect,createBlog);
+router.get("/blog",getAllBlogs);
+router.get("/blog/:id",protect,getBlogById);
+router.put("/blog/:id",protect,updateBlog);
+router.delete("/blog/:id",protect,deleteBlog);
 
 module.exports=router;
--- frontend/src/hook/allData.js
+++ frontend/src/hook/allData.js
@@ -1,6 +1,11 @@
 import { useContext } from "react";
 import { DataProvider } from "../context/DataContext";
+import { BlogContext } from "../context/BlogContext";
 // Custom hook for easy usage
-export default function useData() {
+export function useData() {
   return useContext(DataProvider);
+}
+
+export function useBlog() {
+  return useContext(BlogContext);
 }
